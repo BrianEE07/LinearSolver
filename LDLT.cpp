adabed60fe1ebd28b989bc6391064cea25bcf74e
范詠為 <fanyungwei@ip71-122.vpnip.cc.ntu.edu.tw>
2021-12-16 02:10:28 +0800

    gen tb patterns, float2fix2bin

--- LDLT.cpp
+++ LDLT.cpp
@@ -2,10 +2,24 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <bitset>
 #include "fpm/fixed.hpp"
 #include "fpm/ios.hpp"
 
 using namespace std;
+
+/* LDLT.cpp
+   Specify First
+   -> matrix size : N (NxN)
+   -> input floating point matrix file path : in_path
+   -> output binary fixed point matrix file path : out_mat_path
+   -> output binary fixed point golden file path : out_gold_path
+   Binary Data Format
+   -> 32bits signed fixed point Q15.16, low triangle, column-wise
+   Features
+   -> dump verilog binary testbench patterns
+   -> calculate error between floating point & fixed point (TODO)
+*/
 
 float** LDLT_float (float **A, int N) {
     float **L = new float*[N];
@@ -59,13 +73,48 @@ fpm::fixed_16_16** LDLT_fixed (fpm::fixed_16_16 **A, int N) {
     return L;
 }
 
+void dumpFixed2Bin(string path, fpm::fixed_16_16 **M, int N) {
+    fstream fout;
+    fout.open(path, ios::out);
+
+    // dump binary vertically to file
+    for (int j = 0;j < N;++j) {
+        for (int i = j;i < N;++i) {
+            int l_int = int(float(M[i][j]) * 65536);
+            string l_str = bitset<32>(l_int).to_string();
+            fout << l_str.substr(0, 16) << '_' << l_str.substr(16) << '\n';
+        }
+    }
+
+    // dump comments to file
+    fout << '\n';
+    for (int i = 0;i < N;++i) {
+        fout << "//";
+        for (int j = 0;j <= i;++j) {
+            fout << setw(12) << right << setprecision(5) << fixed << M[i][j];
+
+        }
+        fout << '\n';
+    } 
+    fout << '\n';
+    fout << "// dump file: " << path << '\n';
+    fout << "// data num: " << N * (N + 1) / 2 << '\n';
+    fout << "// 32bits signed fixed point Q15.16" << '\n';
+    fout << "// column-wise" << '\n';
+    fout.close();
+}
+
 int main () {
 
-    int N = 6;
-    // int N = 858;
+    const int N = 6;
+    // const int N = 858;
+    string in_path = "./pattern/matrix_6x6_float.dat";
+    // string in_path = "./pattern/matrix_858x858_float.dat";
+    string out_mat_path = "./pattern/matrix_6x6.dat";
+    string out_gold_path = "./pattern/golden_6x6.dat";
+
     fstream fin;
-    fin.open("./pattern/matrix_6x6_c.dat", ios::in);
-    // fin.open("./pattern/matrix_858x858_c.dat", ios::in);
+    fin.open(in_path, ios::in);
     if (!fin) {
         cerr << "Can't open file!" << endl;
         exit(1);
@@ -73,7 +122,7 @@ int main () {
 
     float **A_float = new float*[N];
     fpm::fixed_16_16 **A_fixed = new fpm::fixed_16_16*[N];
-    // Fix16 **A_fixed = new Fix16*[N];
+
     for (int i = 0;i < N;++i) {
         A_float[i] = new float[N];
     }
@@ -81,6 +130,7 @@ int main () {
         A_fixed[i] = new fpm::fixed_16_16[N];
     }
 
+    // readfile
     string data;
     int cnt = 0;
     while (getline(fin, data, ',')) {
@@ -90,20 +140,27 @@ int main () {
     }
     fin.close();
 
+    // LDLT
     float** L_float = LDLT_float(A_float, N);
     fpm::fixed_16_16** L_fixed = LDLT_fixed(A_fixed, N);
 
+    // dumpfile
+    dumpFixed2Bin(out_mat_path, A_fixed, N);
+    dumpFixed2Bin(out_gold_path, L_fixed, N);
+
+    // print if needed
+    cout << "FLOAT LDLT" << endl;
     for (int i = 0;i < N;++i) {
         for (int j = 0;j < N;++j) {
             cout << setprecision(8) << fixed << L_float[i][j] << " ";
         }
         cout << endl;
     }
+    cout << "FIXED LDLT" << endl;
     for (int i = 0;i < N;++i) {
         for (int j = 0;j < N;++j) {
-            // cout << setprecision(8) << fixed << L_float[i][j] << " ";
             cout << setprecision(8) << fixed << L_fixed[i][j] << " ";
         }
         cout << endl;
-    }  
+    }
 }
